b50301e80b382355c5518cf2713d403b0ad2ae8f
K-Town182 <kathrynncraig@student.purdueglobal.edu>
2025-07-23 22:45:39 -0500

    Initial Commit
    * Upload full ASP.NET Core MVC project for Books & Brew
    * Includes drink builder steps, CSS styling, image assets, and page layout
    * Base layout and core functionality added- more to come

--- obj/Debug/net8.0/PaperAndProse.AssemblyInfo.cs
+++ obj/Debug/net8.0/PaperAndProse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperAndProse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874a44b60103771584704f6d0427c865a800114c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperAndProse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperAndProse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
